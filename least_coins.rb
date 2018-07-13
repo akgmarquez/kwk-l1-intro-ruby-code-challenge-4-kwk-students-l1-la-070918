0593a92e6563c1e08e6e690c948e7791a6ccc1d4
akgmarquez <akgmarquez@gmail.com>
2018-07-13 04:57:48 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,29 @@
 #write out your code here
 
 def least_coins(cents)
+  coin_types = {
+    
+    :quarters => 0
+    :dimes => 0
+    :nickels => 0
+    :pennies => 0
 
-#Code your answer here!
-
-end
+  }
+  
+  quarters = cents/25
+  if quarters == 0
+    puts ":quarters => 0"
+  else quarters!=0
+    puts coin_types [:quarters] = quarters
+    cents = cents - quarters*25
+  end
+  
+  dimes = cents/10
+  if dimes == 0
+    puts ":dimes => 0"
+  else dimes!=0
+    puts coin_types [:dimes] = dimes
+    cents = cents - dimes*10
+  end
+  
+least_coins
